f06a7e41add3c867e574210e7c209d9d72592d3b
David Poulin <davidopoulin@gmail.com>
2016-06-18 21:17:31 -0400

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('wolski')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('wolski', ['ngAnimate', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
